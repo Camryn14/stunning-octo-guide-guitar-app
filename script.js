c66bdda537bd78902f92c90b5f9b51f7bd5bf885
Camryn14 <149531886+Camryn14@users.noreply.github.com>
2024-07-08 18:17:56 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,38 @@
 // A $( document ).ready() block.
 $( document ).ready(function() {
     console.log( "Hello World" );
+
+
+    $('#btnLoadSports').on('click', function(){
+        
+        console.log(sportList);
+        $('#sportlist').append(sportList);
+       
+
+    });
+
+
+    $('#btnClearSports').on('click', function(){
+
+        $('#sportlist').empty();
+
+    });
+
+
 });
+
+
+
+let sportList = [
+
+ "Basketball",
+ "Baseball",
+ "Football"
+
+];
+
+
+//var el;
+//el.document.getElementByTagname('button')
+//el.addEventListener('click', 
+
